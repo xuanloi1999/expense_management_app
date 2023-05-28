3f82fc6824b2db4b853588c0e4ee8c22c51a7ce4
xuanloi1999 <xuanloi3135@gmail.com>
2023-05-28 16:44:39 +0700

    Insert, update, get by name

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require("express")
 const mongoose = require("mongoose")
 const User = require("./models/users")
-
+const Category = require("./models/category")
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -24,9 +24,10 @@ const connectDB = async () => {
 }
 
 app.get('/', (req, res) => {
-    res.send({title: 'Test'})
+    res.send({App: 'Expensive Management App'})
 })
 
+//User
 app.post('/user/add', async (req, res) => {
     const user = req.body
     try {
@@ -41,6 +42,37 @@ app.get('/user', async (req, res) => {
     const username = req.query.username
     const user = await User.find({username: username});
     return user? res.json(user): res.send("Something error")
+})
+
+//category
+
+app.post('/category/add', async (req, res) => {
+    const categoryParams = req.body
+    categoryParams.slug = req.body.name.replace(' ', '-')+ '-' +  Math.floor(Date.now() / 1000);;
+
+    try {
+        await Category.insertMany([categoryParams])
+        res.send("Add category complete")
+    } catch (error) {
+        console.log("err" + error);
+    }
+})
+
+app.post('/category/update', async (req, res) => {
+    const categoryParams = req.body
+    try {
+        await Category.updateOne({slug: categoryParams.slug}, categoryParams)
+        res.send("Update Category completely")
+    } catch (error) {
+        console.log("err" + error);
+    }
+})
+
+app.get('/category', async (req, res) => {
+    const categoryName = req.query.name
+    const regex = new RegExp(`.*${categoryName}\\w*`);
+    const category = await Category.find({name: {$regex: regex}});
+    return category? res.json(category): res.send("Something error")
 })
 
 
