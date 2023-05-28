499353584fd71c6dbd7ab1fde7b78590ef622d96
xuanloi1999 <xuanloi3135@gmail.com>
2023-05-28 09:10:05 +0700

    add some field for User Schema

--- models/users.js
+++ models/users.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 const userSchemna = new Schema({
     username: {type: String, require: true},
-    password: {type: String, require: true}
+    password: {type: String, require: true},
+    createdAt: {type: Date, default: Date.now},
+    updateAt: {type: Date, default: Date.now},
+    deletedAt: {type: Date}
 })
 
 module.exports = mongoose.model("User", userSchemna)
