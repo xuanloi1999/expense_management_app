9c5476fc3bd7aa9988c32255d516a05dfb5f669c
xuanloi1999 <xuanloi3135@gmail.com>
2023-05-28 16:42:21 +0700

    add third party for category

--- models/category.js
+++ models/category.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose')
+const slug = require('mongoose-slug-updater')
+mongoose.plugin(slug);
 
 const Schema = mongoose.Schema;
 const categorySchemna = new Schema({
     name: {type: String, require: true},
+    color: {type:String},
     thumbnail: {type: String},
+    slug: {type: String},
     createdAt: {type: Date, default: Date.now},
     updateAt: {type: Date, default: Date.now},
     deletedAt: {type: Date}
 })
 
-module.exports = mongoose.model("User", userSchemna)
+module.exports = mongoose.model("Category", categorySchemna)
