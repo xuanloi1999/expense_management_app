ec311a39ab6b120ea9b493fc580e39fe9023f44c
xuanloi1999 <xuanloi3135@gmail.com>
2023-06-10 00:50:25 +0700

    update account model

--- models/account.js
+++ models/account.js
@@ -5,9 +5,8 @@ const accountSchemna = new Schema({
     username: {type: String, require: true},
     password: {type: String, require: true},
     email: {type: String, require: true},
-    createdAt: {type: Date, default: Date.now},
-    updateAt: {type: Date, default: Date.now},
-    deletedAt: {type: Date}
+    money: {type: String, require:true},
+    createdAt: {type: Date, default: Date.now}
 })
 
 module.exports = mongoose.model("Account", accountSchemna)
