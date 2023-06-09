c53e45114a00197866df110b7657f61546131301
xuanloi1999 <xuanloi3135@gmail.com>
2023-06-10 01:27:42 +0700

    solve conflict

--- models/transaction.js
+++ models/transaction.js
@@ -1,17 +1,12 @@
 const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema;
-const transactionSchemna = new Schema({
-    id: {type: String, default: new mongoose.mongo.ObjectId()},
-    money: {type: String, require: true},
-    category: {type:String},
-    transactionAt: {type: Date},
-    note: {type:String},
-    thubnail: {type: String},
-    slug: {type: String},
-    createdAt: {type: Date, default: Date.now},
-    updateAt: {type: Date, default: Date.now},
-    deletedAt: {type: Date}
+const TransactionSchemna = new Schema({
+    expense: {type: String, require: true},
+    category: {type: String, require: true},
+    transactionAt: {type: Date, require: true},
+    note: {type: String},
+    typeTransaction: {type: String},
+    accountID: {type: String},
+    groupID: {type: String},
 })
-
-module.exports = mongoose.model("Transaction", transactionSchemna)
