bda7e91a1001d1e93e1f3ba37090bc5dc9b2f774
xuanloi1999 <xuanloi3135@gmail.com>
2023-06-10 02:26:38 +0700

    Update category model

--- models/category.js
+++ models/category.js
@@ -3,9 +3,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 const categorySchemna = new Schema({
     name: {type: String, require: true},
-    createdAt: {type: Date, default: Date.now},
-    updateAt: {type: Date},
-    deletedAt: {type: Date}
+    icon: {type: String},
+    typeCategory: {type: String}
 })
 
 module.exports = mongoose.model("Category", categorySchemna)
